a4f9aebc712a8115a84dfba74e446f4f56e7958a
Sangmeshwar19 <ganub964@gmail.com>
2022-12-02 16:16:07 +0530

    Initial commit

---
+++ src/DeluxePizza.java
@@ -0,0 +1,11 @@
+
+    public class DeluxePizza extends Pizza {
+
+        public DeluxePizza(Boolean isVeg) {
+            super(isVeg);
+            // your code goes here
+            addExtraCheese();
+            addExtraToppings();
+        }
+    }
+
